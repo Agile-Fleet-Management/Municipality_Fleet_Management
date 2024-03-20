545528f5a3fe533a3546e58ed727d28d289897e9
yassib1 <86356206+yibork@users.noreply.github.com>
2024-03-20 13:22:23 +0000

    update

--- Municipality_fleet_management/settings.py
+++ Municipality_fleet_management/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users',
-    'Vehicles',
+    'apps.users',
+    'apps.Vehicles',
 ]
 
 MIDDLEWARE = [
