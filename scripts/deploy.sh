93bac247a259bdb14bb2aed2c438e980fea58cae
Douae-Kbl <kabelma.douae@gmail.com>
2024-03-23 12:41:36 +0000

    popi

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,10 @@ echo "Pulling latest API changes"
 
 git pull
 
+echo "Stopping and removing existing containers"
+
+docker-compose down
+
 echo "Building latest changes"
 
 docker compose up -d --build
