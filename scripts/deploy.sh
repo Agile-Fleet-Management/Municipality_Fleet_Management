d71290a8a349c2aded24f57d3da4b0d94e7a69b2
Douae-Kbl <kabelma.douae@gmail.com>
2024-03-24 18:05:48 +0000

    trying another dpeloy fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
-echo "Changing branch to main"
-
-git checkout main
-
-echo "Stashing any existing changes"
-
-git stash
-
-echo "Pulling latest API changes"
-
-git pull
-
 echo "Stopping and removing existing containers"
 
 docker compose down
