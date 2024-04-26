881d19923a9b28de828bb863bd1369528a3749ed
Douae-Kbl <kabelma.douae@gmail.com>
2024-04-26 11:28:03 +0100

    modified home page view

--- backend/Municipality_fleet_management/views.py
+++ backend/Municipality_fleet_management/views.py
@@ -1,4 +1,8 @@
-from django.http import HttpResponse
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
+@api_view(['GET'])
+@permission_classes([AllowAny])
 def home(request):
-    return HttpResponse('App demo')
+    return Response({"message": "This is the home page accessible to anyone."})
