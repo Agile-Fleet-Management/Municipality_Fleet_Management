a6d01379d1446e92e7df81da61e49000dad9de94
yassib1 <86356206+yibork@users.noreply.github.com>
2024-05-08 15:30:22 +0100

    update

--- backend/Municipality_fleet_management/settings.py
+++ backend/Municipality_fleet_management/settings.py
@@ -114,7 +114,6 @@ ALLOWED_HOSTS=['*']
 CSRF_TRUSTED_ORIGINS = [
     'https://www.agile.fixi.website',
     'https://agile.fixi.website',
-    'localhost:3000',
     'http://localhost:3000',
 ]
 USE_X_FORWARDED_HOST = True
